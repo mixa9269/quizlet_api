be21fd611857be6b00451fafd628efc490ca9402
Mikhail Kaminsky <kam.m.a@ya.ru>
2020-03-13 18:08:15 +0300

    Rework users#create to process_outcome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,9 @@ class ApplicationController < ActionController::API
   include SessionsHelper
   include ErrorRescuable
 
-  def render_single_outcome(outcome, serializer)
-    if outcome.valid?
-      render json: outcome.result, serializer: serializer
-    else
-      render json: outcome.errors, status: :bad_request
-    end
-  end
-
   def process_outcome(outcome)
     if outcome.invalid?
-      render json: outcome.errors, status: :bad_request if outcome.invalid?
+      render json: outcome.errors, status: :bad_request
     elsif block_given?
       yield outcome.result
     end
