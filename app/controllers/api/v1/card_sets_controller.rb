7b7f73f912255cfe8e31ee24b16d5ab3e24700ca
Mikhail Kaminsky <kam.m.a@ya.ru>
2020-03-13 17:20:38 +0300

    Refactoring card_sets#create

--- app/controllers/api/v1/card_sets_controller.rb
+++ app/controllers/api/v1/card_sets_controller.rb
@@ -6,23 +6,15 @@ module Api
       before_action :authenticate
 
       def create
-        outcome = CardSets::Create.run(card_set_params)
-        render_single_outcome(outcome, CardSetSerializer)
+        process_outcome(CardSets::Create.run(params)) do |result|
+          render json: result, status: :created
+        end
       end
 
       def destroy
         process_outcome(CardSets::Destroy.run(params)) do |result|
           render json: result, status: :ok
         end
-      end
-
-      private
-
-      def card_set_params
-        {
-          title: params[:title],
-          user: current_user
-        }
       end
     end
   end
--- app/interactions/card_sets/create.rb
+++ app/interactions/card_sets/create.rb
@@ -2,15 +2,23 @@
 
 module CardSets
   class Create < ActiveInteraction::Base
+    object :current_user, class: 'User'
     string :title
-    object :user
 
     def execute
-      outcome = user.card_sets.create(inputs)
+      outcome = current_user.card_sets.create(new_params)
 
       errors.merge!(outcome.errors) unless outcome.save
 
       outcome
+    end
+
+    private
+
+    def new_params
+      {
+        title: title
+      }
     end
   end
 end
